77da4e49bcf1869481f547a74c4e6598305a5c70
alanatm <roalan96@gmail.com>
2024-08-27 22:07:15 -0600

    se agregan imágenes y funciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,44 @@
-let texto = document.getElementById('input-texto').value;
-let cifrado = document.getElementById('text-encriptado');
-let cifrar = document.getElementById('cifrar');
-let descifrar = document.getElementById('descifrar');
-let copiar = document.getElementById('copiar')
+let texto = document.getElementById("input-texto").value;
+let cifrado = document.getElementById("text-encriptado");
+let cifrar = document.getElementById("cifrar");
+let descifrar = document.getElementById("descifrar");
+let copiar = document.getElementById("copiar");
 
-cifrar.addEventListener('click',()=>{
- e.preventDefault();
+if (texto != "") {
+  cifrar.addEventListener("click", () => {
     let textocifrado = texto
-    .replace(/a/gi, "agu")
-    .replace(/e/gi, "jam")
-    .replace(/i/gi, "tor")
-    .replace(/o/gi, "tip")
-    .replace(/u/gi, "cra");
-
+      .replace(/a/gi, "agu")
+      .replace(/e/gi, "jam")
+      .replace(/i/gi, "tor")
+      .replace(/o/gi, "tip")
+      .replace(/u/gi, "cra");
     cifrado.innerHTML = textocifrado;
-    
-})
+    document.getElementById('imagen').style.display='none';
+    document.getElementById('notexto').style.display='none';
+  });
 
-descifrar.addEventListener('click',()=>{
-    
+  descifrar.addEventListener("click", () => {
     let textocifrado = texto;
     cifrado.innerHTML = textocifrado;
-    
-})
+    document.getElementById('imagen').style.display='none';
+    document.getElementById('notexto').style.display='none';
+  });
 
-copiar.addEventListener('click',()=>{
-    navigator.clipboard.writeText(cifrado.textContent);
-    copiar.textContent="¡Copiado!";
-    setTimeout(()=>{
-        copiar.textContent="Copiar";
-    },2000)
-        
-})
+} else {
+  cifrar.addEventListener("click", () => {
+    alert("No has escrito algún texto");
+    location.reload();
+  });
+  descifrar.addEventListener("click", () => {
+    alert("No has escrito algún texto");
+    location.reload();
+  });
+}
+
+copiar.addEventListener("click", () => {
+  navigator.clipboard.writeText(cifrado.textContent);
+  copiar.textContent = "¡Copiado!";
+  setTimeout(() => {
+    copiar.textContent = "Copiar";
+  }, 2000);
+});
